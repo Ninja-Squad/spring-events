e38f6bcc463f49cd78066dd380ec5d1b2a42d540
Cyril Lacôte <cyril.lacote@gmail.com>
2014-09-19 10:24:14 +0200

    Javadoc typos

--- src/main/java/com/ninja_squad/spring/events/EventFirer.java
+++ src/main/java/com/ninja_squad/spring/events/EventFirer.java
@@ -25,7 +25,7 @@ package com.ninja_squad.spring.events;
 
 /**
  * Allows firing an event that will be received by event observing methods, i.e. methods annotated with
- * {@link Observes}. Note that a bean of type EventObserverBeanPostProcessor, which implements this interface,
+ * {@link Observes}. Note that a bean of type {@link EventObserverBeanPostProcessor}, which implements this interface,
  * must be added to the application context for this mechanism to work. This is best done by using the
  * {@link EnableEvents} annotation.
  * @author JB Nizet
